2b51497a8ebf088ef68060e2f99c47c2cd631558
jqqjj <jqqjj@qq.com>
2022-01-05 11:00:37 +0800

    Update JudgeRobot.php

--- src/Facades/JudgeRobot.php
+++ src/Facades/JudgeRobot.php
@@ -3,7 +3,25 @@
 namespace Jqqjj\LaravelSimpleJudgeRobot\Facades;
 
 use Illuminate\Support\Facades\Facade;
+use Jqqjj\LaravelSimpleJudgeRobot\Session;
 
+/**
+ * Class JudgeRobot
+ * @package Jqqjj\LaravelSimpleJudgeRobot\Facades
+ *
+ * @method static void attemptFailure()
+ * @method static void attemptSuccess()
+ * @method static bool isHuman()
+ * @method static bool isRobot()
+ * @method static void trashData()
+ * @method static string getCookieKey()
+ * @method static string getSessionId()
+ * @method static Session getSession()
+ * @method static string getOutputCookieString()
+ * @method static void outputCookie()
+ *
+ * @see \Jqqjj\LaravelSimpleJudgeRobot\Manager
+ */
 class JudgeRobot extends Facade
 {
     protected static function getFacadeAccessor()
